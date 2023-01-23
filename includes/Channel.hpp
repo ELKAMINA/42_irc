0caacddcc4c2f0bab4196bc5caee961a214df4b6
jcervoni <jcervoni@student.42.fr>
2023-01-23 16:20:25 +0100

    WIP CHANNEL :
    Implementing class Channel, prototypes and attributes:
    actions in channel will be managed by object Channel (remove/add
    users in appropriate vectors of Unique nicknames);
    Implementation of boolean getters for checking of member'status;

---
+++ includes/Channel.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 15:06:37 by jcervoni          #+#    #+#             */
+/*   Updated: 2023/01/23 16:14:26 by jcervoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+# include <vector>
+# include <iostream>
+# include <string>
+# include <map>
+# include "Colors.hpp"
+# include "ServerSocket.hpp"
+# include "Request.hpp"
+# include "Client.hpp"
+
+class Channel
+{
+	public:
+
+	/* CONSTRUCTORS */
+		Channel( std::vector<Client>& allUsers, std::string channelName, Client& owner );
+		Channel( std::vector<Client>& allUsers, std::string channelName, std::string channelKey, Client& owner );
+		Channel( const Channel& rhs );
+		~Channel();
+
+	/* OPERATORS OVERLOAD */
+		Channel &operator=( const Channel& rhs );
+	
+	/* METHODS */
+
+		/* RIGHTS & STATUS */
+		void addUser(Client &user);
+		void deleteUser(Client &user);
+		void addOperator(Client &user);
+		void deleteOperator(Client &user, std::string fault);
+		void ban(Client &user, std::string fault);
+		void inviteIn(Client& inviter, Client &invited);
+		void authorizeVocal(Client &user);
+		void mute(Client &user);
+
+		/* COMMUNICATION*/
+		void sendMessageToMembers(Message &message, Client &from);
+	/* CHAN INFO GETTERS */
+
+		bool isMember(Client &user);
+		bool isOperator(Client &user);
+		bool isBanned(Client &user);
+		bool canTalk(Client &user);
+	private:
+
+		char							_prefix;
+		int								_onlineUsers;
+		std::string						_name;
+		std::string						_key;
+		std::string						_topic;
+		std::vector<std::string>		_users;
+		std::vector<std::string>		_operators; // separated from users or duplicated ?
+		std::vector<std::string>		_vocal;
+		std::vector<std::string>		_banned;
+		std::vector<Client>&			_allUsers;
+		// vector<char>					_mods; a définir
+
+};
+
+#endif
