826ba405d176d290e83ae1bcf81bbc9d05098fa9
ael-khat <ael-khat@42.student.fr>
2023-01-26 16:34:27 +0100

    Ajout fct Error Amina

--- includes/numeric_replies/numeric_replies.hpp
+++ includes/numeric_replies/numeric_replies.hpp
@@ -15,10 +15,15 @@
 
 # include "Client.hpp"
 # include "Channel.hpp"
+# include <sstream>
 
 void	errAlreadyInUse(std::string serverName, std::string nickName);
 void	errNoSuchNick(std::string nickName);
 void	errNoSuchChannel(std::string channelName);
 void	errCannotSendToChan(std::string channelName);
+std::string			errNeedMoreParams(Client *cli, Request *req);
+std::string			errPasswMismatch(Client *cli, Request *req); // Server client need to be disconected from the server after this
+std::string			errAlreadyRegistered(Client *cli, Request *req);
+
 
 #endif
