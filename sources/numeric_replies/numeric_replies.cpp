920feda21827ffec899a203d684e8579993f1e18
ael-khat <ael-khat@42.student.fr>
2023-01-26 19:10:27 +0100

    Amina: Numeric_replie for cmd NICK

--- sources/numeric_replies/numeric_replies.cpp
+++ sources/numeric_replies/numeric_replies.cpp
@@ -15,10 +15,10 @@
 // all prints on std streams will be replaced by a message object which will
 // be sent to client
 
-void	errAlreadyInUse(std::string serverName, std::string nickName)
-{
-	std::cout << serverName << " 433 * " << nickName << " :Nickname already in use" << std::endl;
-}
+// void	errAlreadyInUse(std::string serverName, std::string nickName) // Amina: doublon avec celle d'en bas, je me permets de la commenter pr mes tests
+// {
+// 	std::cout << serverName << " 433 * " << nickName << " :Nickname already in use" << std::endl;
+// }
 
 void	errNoSuchNick(std::string nickName)
 {
@@ -34,22 +34,45 @@ void	errCannotSendToChan(std::string channelName)
 	std::cout << "404 * " << channelName << " :Cannot send to channel" << std::endl;
 }
 
+std::string	errNoNicknameGiven(Client *cli, Request *req)
+{
+	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
+	(void)cli;
+	(void)req; /* le temps de comprendre a quoi correspond <client>*/
+	std::ostringstream oss;
+	oss << "431 * " << ":No nickname given\n";
+	std::string var = oss.str();
+	return (var);
+}
+
+std::string	errErroneusNickname(Client *cli, Request *req)
+{
+	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
+	(void)cli;
+	(void)req; /* le temps de comprendre a quoi correspond <client>*/
+	std::ostringstream oss;
+	oss << "432 * " << cli->getNickName() << " :Erroneous nikckname\n";
+	std::string var = oss.str();
+	return (var);
+}
+
+std::string	errNicknameInUse(Client *cli, Request *req)
+{
+	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
+	(void)cli;
+	(void)req; /* le temps de comprendre a quoi correspond <client>*/
+	std::ostringstream oss;
+	oss << "433 * " << cli->getNickName() << " :Nickname is already in use\n";
+	std::string var = oss.str();
+	return (var);
+}
+
 std::string	errNeedMoreParams(Client *cli, Request *req)
 {
 	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
 	(void)cli; /* le temps de comprendre a quoi correspond <client>*/
 	std::ostringstream oss;
-	oss << "461 * " << "Unregistered Client" << " " << req->_command << " :Not enough parameters\n";
-	std::string var = oss.str();
-	return (var);
-}
-
-std::string	errPasswMismatch(Client *cli, Request *req) // Modifier "unregistered client par le vrai client user"
-{
-	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
-	(void)cli; /* le temps de comprendre a quoi correspond <client>*/
-	std::ostringstream oss;
-	oss << "464 * " << "Unregistered Client" << " " << req->_command << " :Password incorrect\n";
+	oss << "461 * " << req->_command << " :Not enough parameters\n";
 	std::string var = oss.str();
 	return (var);
 }
@@ -60,7 +83,17 @@ std::string	errAlreadyRegistered(Client *cli, Request *req)
 	(void)cli;
 	(void)req; /* le temps de comprendre a quoi correspond <client>*/
 	std::ostringstream oss;
-	oss << "462 * " << "Unregistered Client : You may not reregister\n";
+	oss << "462 * " << "Unauthorized command\n";
+	std::string var = oss.str();
+	return (var);
+}
+
+std::string	errPasswMismatch(Client *cli, Request *req) // Modifier "unregistered client par le vrai client user"
+{
+	// std::cout << "" << cli->getNickName() << req->_command << " :Not enough parameters" << std::endl;
+	(void)cli; /* le temps de comprendre a quoi correspond <client>*/
+	std::ostringstream oss;
+	oss << "464 * " << " :Password incorrect\n";
 	std::string var = oss.str();
 	return (var);
 }
