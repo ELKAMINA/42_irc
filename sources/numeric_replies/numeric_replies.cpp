76e3531b9823f4cd4b60ee6b349b19af6ab485b6
Jean-michel CERVONI <jcervoni@student.42.fr>
2023-02-02 14:48:34 +0100

    FIXIN NUMERIC REPLIES :
    replies are correclty formated, still got to check errors;

--- sources/numeric_replies/numeric_replies.cpp
+++ sources/numeric_replies/numeric_replies.cpp
@@ -6,7 +6,7 @@
 /*   By: jcervoni <jcervoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 17:48:06 by jcervoni          #+#    #+#             */
-/*   Updated: 2023/02/01 12:50:31 by jcervoni         ###   ########.fr       */
+/*   Updated: 2023/02/02 14:40:30 by jcervoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ std::string	rpl_topic(std::string channel, std::string topic ) // Modifier "unre
 {
 	// (void)topic;
 	std::ostringstream oss;
-	oss << channel << ":" << topic << "\n";
+	oss << channel << " :" << topic << "\n";
 	std::string var = oss.str();
 	return (var);
 }
@@ -46,7 +46,7 @@ std::string	rpl_notopic(std::string channel, std::string topic ) // Modifier "un
 {
 	(void)topic;
 	std::ostringstream oss;
-	oss << channel << ":No topic is set" << "\n";
+	oss << channel << " :No topic is set" << "\n";
 	std::string var = oss.str();
 	return (var);
 }
@@ -73,38 +73,32 @@ std::string	rpl_endofnames(std::string channel, std::string op ) // Modifier "un
 {
 	/* Résultat attendu : user per channels prend en compte les channels privés et secrets*/
 	(void)op;
-	std::ostringstream oss;
-	oss << channel << " " << "End of NAMES list\n";
-	std::string var = oss.str();
-	return (var);
+	string reply;
+	reply = channel + " :End of NAMES list\n";
+	return (reply);
 }
 
-std::string	rpl_inviting(std::string channel, std::string nickname ) // Modifier "unregistered client par le vrai client user"
+std::string	rpl_inviting(std::string sender, string target, std::string channel) // Modifier "unregistered client par le vrai client user"
 {
-	/* Résultat attendu : <channel> <nick>*/
-	std::ostringstream oss;
-	oss << channel << " " << nickname;
-	std::string var = oss.str();
-	return (var);
+	string reply;
+	reply = sender + " INVITE " + target + " #" + channel + '\n';
+	return (reply);
 }
 
-std::string	rpl_away(std::string nickname, std::string away ) // Modifier "unregistered client par le vrai client user"
+std::string	rpl_away(std::string nickname, std::string message ) // Modifier "unregistered client par le vrai client user"
 {
 	/* Résultat attendu : <nick>:<away> */
-	std::ostringstream oss;
-	oss << nickname << " " << away;
-	std::string var = oss.str();
-	return (var);
+	string reply;
+	reply = nickname + " :" + message + '\n';
+	return (reply);
 }
 
-std::string	rpl_list(std::string message, std::string op ) // Modifier "unregistered client par le vrai client user"
+std::string	rpl_list(std::string channel, std::string topic ) // Modifier "unregistered client par le vrai client user"
 {
 	/* Résultat attendu : <channel> <#visible>:<topic> */
-	(void)op;
-	std::ostringstream oss;
-	oss << message;
-	std::string var = oss.str();
-	return (var);
+	string reply;
+	reply = "#" + channel + " :" + topic + '\n';
+	return (reply);
 }
 
 std::string	rpl_listend(std::string message , std::string op ) // Modifier "unregistered client par le vrai client user"
@@ -112,8 +106,7 @@ std::string	rpl_listend(std::string message , std::string op ) // Modifier "unre
 	/* Résultat attendu : <channel> <#visible>:<topic> */
 	(void)op;
 	(void)message;
-	std::ostringstream oss;
-	oss << "End of list\n";
-	std::string var = oss.str();
-	return (var);
+	string reply;
+	reply = ":End of list\n";
+	return (reply);
 }
